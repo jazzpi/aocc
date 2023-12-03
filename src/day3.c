1d7eb889bfcd7d57c951e01c3b6c0ef4e561632f
jazzpi <jasper@mezzo.de>
2023-12-03 20:05:34 +0100

    Day 3, Part 2

--- src/day3.c
+++ src/day3.c
@@ -44,7 +44,14 @@ int is_part_number(size_t line_no, regmatch_t* match, const char** lines,
   return 0;
 }
 
-void part1(const char** lines, size_t nlines) {
+typedef struct {
+  size_t row;
+  size_t start_col;
+  size_t end_col;
+  int value;
+} part_no;
+
+part_no* find_part_numbers(const char** lines, size_t nlines, size_t* n_parts) {
   assert(nlines > 0);
   size_t linelen = strlen(lines[0]);
 
@@ -54,7 +61,10 @@ void part1(const char** lines, size_t nlines) {
     exit(1);
   }
 
-  size_t sum = 0;
+  *n_parts = 0;
+  size_t buf_size = 128;
+  part_no* numbers = malloc(sizeof(*numbers) * buf_size);
+
   for (size_t l = 0; l < nlines; l++) {
     assert(strlen(lines[l]) == linelen);
     size_t nmatches;
@@ -62,8 +72,20 @@ void part1(const char** lines, size_t nlines) {
     for (size_t n = 0; n < nmatches; n++) {
       if (is_part_number(l, &matches[n], lines, nlines, linelen)) {
         char* str = re_match_dup(lines[l], &matches[n]);
-        sum += atoi(str);
+        int value = atoi(str);
         free(str);
+
+        if (*n_parts == buf_size) {
+          buf_size *= 2;
+          numbers = realloc(numbers, sizeof(*numbers) * buf_size);
+        }
+
+        part_no* no = &numbers[*n_parts];
+        *n_parts += 1;
+        no->row = l;
+        no->start_col = matches[n].rm_so;
+        no->end_col = matches[n].rm_eo - 1;
+        no->value = value;
       }
     }
     free(matches);
@@ -71,7 +93,72 @@ void part1(const char** lines, size_t nlines) {
 
   regfree(&re);
 
+  return numbers;
+}
+
+void part1(const char** lines, size_t nlines) {
+  size_t n_parts;
+  part_no* numbers = find_part_numbers(lines, nlines, &n_parts);
+  size_t sum = 0;
+  for (size_t i = 0; i < n_parts; i++) {
+    sum += numbers[i].value;
+  }
+  free(numbers);
+
   printf("Sum of part numbers: %zu\n", sum);
+}
+
+part_no* part_at(size_t row, size_t col, part_no* parts, size_t n_parts) {
+  for (size_t i = 0; i < n_parts; i++) {
+    part_no* part = &parts[i];
+    if (part->row == row && part->start_col <= col && part->end_col >= col) {
+      return part;
+    }
+  }
+
+  return NULL;
+}
+
+void part2(const char** lines, size_t nlines) {
+  size_t n_parts;
+  part_no* parts = find_part_numbers(lines, nlines, &n_parts);
+
+  size_t linelen = strlen(lines[0]);
+
+  int sum = 0;
+
+  for (int row = 0; row < (int)nlines; row++) {
+    for (int col = 0; col < (int)linelen; col++) {
+      if (lines[row][col] != '*') {
+        continue;
+      }
+      int gear_ratio = 1;
+      int parts_adjacent = 0;
+      part_no* prev_part = NULL;
+      for (int r = row - 1; r <= row + 1; r++) {
+        for (int c = col - 1; c <= col + 1; c++) {
+          if (r < 0 || r > (int)nlines || c < 0 || c > (int)linelen ||
+              (r == row && c == col)) {
+            continue;
+          }
+          part_no* p = part_at(r, c, parts, n_parts);
+          if (p == NULL || p == prev_part) {
+            continue;
+          }
+          prev_part = p;
+          parts_adjacent++;
+          gear_ratio *= p->value;
+        }
+      }
+      if (parts_adjacent == 2) {
+        sum += gear_ratio;
+      }
+    }
+  }
+
+  free(parts);
+
+  printf("Sum of gear ratios: %d\n", sum);
 }
 
 int main(int argc, char** argv) {
@@ -79,6 +166,8 @@ int main(int argc, char** argv) {
 
   if (part == 1) {
     part1(lines, nlines);
+  } else if (part == 2) {
+    part2(lines, nlines);
   } else {
     usage_abrt(argv[0]);
   }
